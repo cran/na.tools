5c9bbcf3778d064a7b67cb8043fa2e6b85aa370e
Christopher Brown <chris.brown@decisionpatterns.com>
2018-02-12 18:12:33 +0000

    version 0.2.1

---
+++ R/na.most_freq.R
@@ -0,0 +1,21 @@
+#' @details 
+#' 
+#' `na.mode` replaces all `NA` with the most frequently occuring value. In 
+#' the event of ties, the value encounter first is listed first is used.
+#' 
+#' `na.most_freq` is an alias for `na.mode` since the underlying function
+#' 
+#' @examples 
+#'   na.most_freq( c(1,1,NA,4) )
+#'   na.most_freq( c(1,1,4,4,NA) ) 
+#'
+#' @rdname impute-commutative
+#' @md
+#' @export
+
+na.mode <- function(.x, ... ) 
+  na.replace(.x, .na=function(x) most_freq(x, na.omit, ...) )
+
+#' @rdname impute-commutative
+#' @export
+na.most_freq <- na.mode 
